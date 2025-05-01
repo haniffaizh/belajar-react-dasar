1a1eb1af5049fd1f2d3fc7c27f65841328d2c454
Hanif Faiz Hidayat <haniffaizh@gmail.com>
2025-05-01 20:41:45 +0700

    event propagation

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -6,6 +6,8 @@ import TodoList from "../todolist/TodoList.jsx";
 import Table from "../table/Table.jsx";
 import AlertButton from "../button/AlertButton.jsx";
 import MyButton from "../button/MyButton.jsx";
+import Toolbar from "../button/Toolbar.jsx";
+import SearcForm from "../form/SearchForm.jsx";
 
 createRoot(document.getElementById("root"))
     .render(
@@ -16,6 +18,11 @@ createRoot(document.getElementById("root"))
                 <Table/>
                 <AlertButton text="Click Me" message="You click me"/>
                 <MyButton text="Smash me" onSmash={()=>{alert("Click Me")}} />
+                <Toolbar onClick={(e)=>{
+                    e.stopPropagation();
+                    alert("You click toolbar");
+                }}/>
+                <SearcForm/>
             </Container>
         </StrictMode>
     )
